9144352a9e571e617c3fd20d1d7991d8422cd8ab
Krish Rupapara <krishrupapara01@gmail.com>
2023-06-28 12:03:43 +0530

    Final commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,11 @@ import Icon from "./Icon";
 export default function Footer() {
   return (
     <div className="flex items-center w-full h-16 py-4 mt-10 border-t bg-bgPrimary">
-      <div className="flex items-center justify-between w-11/12 mx-auto">
-        <div className="text-lg text-white">Made with ❤️ by Krish Rupapara</div>
+      <div className="flex items-center justify-between w-11/12 gap-4 mx-auto">
+        <div className="text-lg text-white">
+          Made with ❤️
+          <span className="whitespace-nowrap">by Krish Rupapara</span>
+        </div>
         <div className="flex gap-5 mt-3 ml-1l">
           <Icon
             link="https://www.linkedin.com/in/krish-rupapara-6a0832280/"
